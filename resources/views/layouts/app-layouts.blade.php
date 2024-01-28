348b804cf11f016e7789127a035dd68e4c0f275b
Idehen Divine <idehendivine16@gmail.com>
2024-01-28 15:53:17 +0100

    fixed some errors

--- resources/views/layouts/app-layouts.blade.php
+++ resources/views/layouts/app-layouts.blade.php
@@ -4,7 +4,7 @@
 
 
 <html lang="en" class="light-style layout-navbar-fixed layout-menu-fixed layout-compact " dir="ltr"
-    data-theme="theme-default" data-assets-path="assets/" data-template="vertical-menu-template">
+    data-theme="theme-default" data-assets-path="{{ asset('assets') }}/" data-template="vertical-menu-template">
 
 <head>
     <meta charset="utf-8" />
--- resources/views/layouts/auth-layouts.blade.php
+++ resources/views/layouts/auth-layouts.blade.php
@@ -4,7 +4,7 @@
 
 
 <html lang="en" class="light-style layout-navbar-fixed layout-menu-fixed layout-compact " dir="ltr"
-    data-theme="theme-default" data-assets-path="assets/" data-template="vertical-menu-template">
+    data-theme="theme-default" data-assets-path="{{ asset('assets') }}/" data-template="vertical-menu-template">
 
 <head>
     <meta charset="utf-8" />
@@ -71,7 +71,7 @@
     <!-- / Layout wrapper -->
 
 	<x-toast/>
-	
+
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
 
